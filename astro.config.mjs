d750cd6f01f4b47f72b305edee868136259f0e9a
Karen López <lopezreyeskarensofia@gmail.com>
2026-06-02 08:09:54 -0500

    melo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://karensofialopezreyes.github.io/Una_web_estatica/',
+    base: '/Una_web_estatica/',
+    vite: {
+        plugins: [tailwindcss()]
+    }
+});
